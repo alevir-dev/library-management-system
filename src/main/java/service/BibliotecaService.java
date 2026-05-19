9e00e15c10f5ee7dbf034a6f26aa220a1fb2eaa9
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-05-19 17:48:33 -0300

    feat: implementa cadastro de usuarios

--- src/main/java/service/BibliotecaService.java
+++ src/main/java/service/BibliotecaService.java
@@ -1,7 +1,20 @@
 package main.java.service;
 
 import main.java.domain.Biblioteca;
+import main.java.domain.Usuario;
 
 public class BibliotecaService {
     Biblioteca biblioteca;
+
+
+    public void adicionarUsuario(Usuario usuario) {
+
+        biblioteca.getUsuarios()[biblioteca.getPosicaoUsuario()] = usuario;
+
+        biblioteca.setPosicaoUsuario(biblioteca.getPosicaoUsuario() + 1);
+
+        System.out.println("Usuário adicionado com sucesso!");
+    }
+
+
 }
