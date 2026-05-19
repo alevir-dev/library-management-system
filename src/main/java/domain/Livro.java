13923b3f60065a56c6087b8c03d06b00c1d5f668
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-05-19 17:46:12 -0300

    feat: implementa controle de status dos livros

--- src/main/java/domain/Livro.java
+++ src/main/java/domain/Livro.java
@@ -1,15 +1,28 @@
 package main.java.domain;
 
+
+import main.java.enums.StatusLivro;
+
 public class Livro {
+
     private final String titulo;
     private final int paginas;
     private final String autor;
 
+    private StatusLivro status = StatusLivro.DISPONIVEL;
 
     public Livro(String titulo, int paginas, String autor) {
         this.titulo = titulo;
         this.paginas = paginas;
         this.autor = autor;
+    }
+
+    public void setStatus(StatusLivro status) {
+        this.status = status;
+    }
+
+    public StatusLivro getStatus() {
+        return status;
     }
 
     public String getTitulo() {
