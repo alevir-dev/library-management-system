e88734c004c0498e56209ebfd76d113445ddddee
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-05-19 17:39:14 -0300

    feat: implementa classe Livro

--- src/main/java/domain/Livro.java
+++ src/main/java/domain/Livro.java
@@ -1,4 +1,26 @@
 package main.java.domain;
 
 public class Livro {
+    private final String titulo;
+    private final int paginas;
+    private final String autor;
+
+
+    public Livro(String titulo, int paginas, String autor) {
+        this.titulo = titulo;
+        this.paginas = paginas;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
 }
