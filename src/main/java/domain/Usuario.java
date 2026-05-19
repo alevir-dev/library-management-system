c97115259e331f86205d29343d5e9d23e79d0988
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-05-19 17:42:23 -0300

    feat: implementa entidade Usuario

--- src/main/java/domain/Usuario.java
+++ src/main/java/domain/Usuario.java
@@ -1,4 +1,31 @@
 package main.java.domain;
 
 public class Usuario {
+    private final String nome;
+    private final String CPF;
+    private static final int MAX_LIVROS = 10;
+
+    private Livro[] livros = new Livro[MAX_LIVROS];
+    private int posicaoLivros = 0;
+
+    public Usuario(String nome, String CPF) {
+        this.nome = nome;
+        this.CPF = CPF;
+    }
+
+    public Livro[] getLivros() {
+        return livros;
+    }
+
+    public int getPosicaoLivros() {
+        return posicaoLivros;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
 }
