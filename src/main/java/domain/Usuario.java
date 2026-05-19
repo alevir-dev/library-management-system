fd262bc9578deb524e75387e29b9304ba1536018
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-05-19 17:42:59 -0300

    feat: implementa gerenciamento de livros do usuario

--- src/main/java/domain/Usuario.java
+++ src/main/java/domain/Usuario.java
@@ -13,6 +13,39 @@ public class Usuario {
         this.CPF = CPF;
     }
 
+    public void adicionarLivro(Livro livro) {
+        this.livros[posicaoLivros] = livro;
+        posicaoLivros++;
+
+        System.out.println("Livro adicionado com sucesso!");
+    }
+
+    public void removerLivro(String tituloLivro) {
+
+        int salvarPosicao = -1;
+
+        for (int i = 0; i < posicaoLivros; i++) {
+            if (livros[i].getTitulo().equalsIgnoreCase(tituloLivro)) {
+                salvarPosicao = i;
+                break;
+            }
+        }
+
+        if (salvarPosicao == -1) {
+            System.out.println("Título não encontrado!");
+            return;
+        }
+
+        for (int i = salvarPosicao; i < posicaoLivros - 1; i++) {
+            livros[i] = livros[i + 1];
+        }
+
+        livros[posicaoLivros - 1] = null;
+        posicaoLivros--;
+
+        System.out.println("Livro devolvido com sucesso!");
+    }
+
     public Livro[] getLivros() {
         return livros;
     }
